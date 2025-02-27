7a8a45c09cc01fce8c7eb76864d6e70e1fc49e73
Gennady Denisenko <gennadii.denisenko@gmail.com>
2025-02-27 19:14:27 +0400

    Lesson 5.3 Done

--- chapter5/src/App.tsx
+++ chapter5/src/App.tsx
@@ -1,10 +1,35 @@
-import './App.css';
-import { TonConnectButton } from '@tonconnect/ui-react';
+import "./App.css";
+import { TonConnectButton } from "@tonconnect/ui-react";
+import { useMainContract } from "./hooks/useMainContract";
 
-export default function App() {
+function App() {
+  const {
+    contract_address,
+    counter_value,
+    recent_sender,
+    owner_adress,
+    contract_balance,
+  } = useMainContract();
   return (
     <div>
-      <TonConnectButton />
+      <div>
+        <TonConnectButton />
+      </div>
+      <div>
+        <div className='Card'>
+          <b>Our contract Address</b>
+          <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
+          <b>Our contract Balance</b>
+          <div className='Hint'>{contract_balance}</div>
+        </div>
+
+        <div className='Card'>
+          <b>Counter Value</b>
+          <div>{counter_value ?? "Loading..."}</div>
+        </div>
+      </div>
     </div>
   );
 }
+
+export default App;

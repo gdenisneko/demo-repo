07cfc3f813b7f55fb3ff6cd27df7d43b876e3d8d
Gennady Denisenko <gennadii.denisenko@gmail.com>
2025-02-27 10:52:15 +0400

    Structure Changes for chapter 5

--- chapter5/src/main.tsx
+++ chapter5/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// this manifest is used temporarily for development purposes
+const manifestUrl = 'ublic/tonconnect-manifest.json';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
-  </StrictMode>,
+  </TonConnectUIProvider>
 )
